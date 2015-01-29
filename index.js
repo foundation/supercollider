cba1bb44161ee80861fae14e178fd5ce47aa9aed
Geoff Kimball <geoff@zurb.com>
2015-01-28 17:10:44 -0800

    Move handlebars helpers to an external file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var css        = require('css');
 var exec       = require('child_process').exec;
 var fs         = require('fs');
 var glob       = require('glob');
-var handlebars = require('handlebars');
+var handlebars = require('./lib/handlebars');
 var hljs       = require('highlight.js');
 var jsdoc      = require('jsdoc3-parser');
 var marked     = require('marked');
@@ -11,44 +11,6 @@ var path       = require('path');
 var rimraf     = require('rimraf');
 var sass       = require('node-sass');
 var sassdoc    = require('sassdoc');
-
-// Capitalizes the first letter of a string
-handlebars.registerHelper('toUpper', function(str) {
-  return str[0].toUpperCase() + str.slice(1);
-});
-
-// Formats a mixin using a SassDoc mixin object to look like this:
-// @mixin mixinName($param, $param) { }
-handlebars.registerHelper('writeMixin', function(mixin) {
-  var name = mixin['context']['name'];
-  var params = mixin['parameter'];
-
-  var str = '@mixin ';
-  str += name + '(';
-  for (var i in params) {
-    str += '$' + params[i]['name'] + ', ';
-  }
-  str = str.slice(0, -2);
-  str += ') { }';
-
-  return str;
-});
-
-// Converts a markdown string to HTML
-handlebars.registerHelper('md', function(text) {
-  return marked(text);
-});
-
-// Returns content inside the block if any parameters are truthy
-// {{#ifany param1 param2 etc}}{{/ifany}}
-handlebars.registerHelper('ifany', function() {
-  arguments = Array.prototype.slice.call(arguments);
-  var args = arguments.slice(0, -1);
-  var context = arguments.slice(-1)[0];
-  for (var item in args) {
-    if (args[item].length > 0) return context.fn(this);
-  }
-});
 
 // Creates a new instance of Supercollider, which will generate a single static site.
 // options: an object of configuration settings:
