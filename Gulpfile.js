4a95d5ff523169983483d7f94eb9f421cab6f7d1
Geoff Kimball <geoff@zurb.com>
2015-02-24 18:08:34 -0800

    Allow base path of templates to be changed

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,12 @@ gulp.task('default', function() {
     html: './test/scss',
     sass: './test/scss',
     js: './test/js/**/*.js',
+    templates: './test/templates',
     destJSON: null,
     dest: './build'
   });
+});
+
+gulp.task('stream', function() {
+
 });
