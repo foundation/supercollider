b1f03256ddb95e41e90d05290eb7a1121206c30f
Geoff Kimball <geoff@zurb.com>
2015-03-26 17:17:45 -0700

    Add JSDoc processing, sorting doclets by type

--- adapters/js/index.js
+++ adapters/js/index.js
@@ -7,6 +7,25 @@ module.exports = {
     });
   },
   process: function(tree) {
-    return tree;
+    var newTree = {
+      'class': [],
+      'function': [],
+      'member': [],
+      'event': []
+    }
+
+    for (var i in tree) {
+      var item = tree[i];
+
+      if (item.undocumented === true || item.access === 'private') continue;
+
+      var group = item.kind;
+      if (newTree[group]) {
+        if (group === 'member' && item.memberof.indexOf('defaults') < 0) continue;
+        newTree[group].push(item)
+      }
+    }
+
+    return newTree;
   }
 }
