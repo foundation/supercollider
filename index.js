b7f9c2d413e8c2465187abb4d0ffbc07239c5765
Geoff Kimball <geoff@zurb.com>
2015-10-13 16:00:09 -0700

    Add built-in adapters in a separate function call, instead of in the config object

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var path = require('path');
-
 function Supercollider() {
   this.options = {};
   this.adapters = {};
@@ -13,8 +11,4 @@ Supercollider.prototype.build = require('./lib/build');
 Supercollider.prototype.adapter = require('./lib/adapter');
 Supercollider.prototype.config = require('./lib/config');
 
-var s = new Supercollider();
-s.adapter('sass', require(path.join(__dirname, 'adapters', 'sass')));
-s.adapter('js', require(path.join(__dirname, 'adapters', 'js')));
-
-module.exports = s;
+module.exports = new Supercollider();
